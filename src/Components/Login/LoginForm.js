e5da7f56eda5ea38aea19964968fab81c5002acf
Solovic2 <islammohamed323@gmail.com>
2023-08-12 11:28:53 +0300

    add new logo and fix redirect login

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -1,26 +1,22 @@
-import React, { useState, useEffect } from 'react';
-import "./LoginForm.css"
-import { Link, useNavigate } from 'react-router-dom';
-import { useCookies } from 'react-cookie';
+import React, { useState, useEffect } from "react";
+import "./LoginForm.css";
+import { Link, useNavigate } from "react-router-dom";
+import { useCookies } from "react-cookie";
 
 function LoginForm() {
   const navigate = useNavigate();
-  const [username, setUserName] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState("")
-  const [{ cookie }, setCookie] = useCookies(['user']);
-
-
+  const [username, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [ cookie , setCookie] = useCookies(["user"]);
   useEffect(() => {
-
-    if (cookie) {
-      // Redirect to login page if user data is not available
+    if (cookie.user) {
       navigate("/");
       return;
     }
-  }, [cookie, navigate])
+  }, [cookie, navigate]);
 
-  if (cookie) {
+  if (cookie.user) {
     return null;
   }
 
@@ -30,69 +26,79 @@ function LoginForm() {
       username: username,
       password: password,
     };
-    const response = await fetch(
-      `http://localhost:9000/login`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        credentials: 'include',
-        body: JSON.stringify({ data: formData }),
-      }
-    );
+    const response = await fetch(`http://localhost:9000/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify({ data: formData }),
+    });
     if (!response.ok) {
       const errorData = await response.json();
       setError(errorData.error);
     } else {
       const userData = await response.json();
-      setCookie('user', JSON.stringify(userData));
+      setCookie("user", JSON.stringify(userData));
       navigate("/", {
         state: { user: userData },
-      })
+      });
     }
   };
 
   return (
     <>
-      <div className='cover'>
-        <div className='title'>تسجيل الدخول</div>
-        <img src={process.env.PUBLIC_URL + '/logo.png'} className="img-responsive" alt="logo"/>
+      <div className="login">
+        <div className="cover">
+          <img
+            src={process.env.PUBLIC_URL + "/banner.png"}
+            className="img-responsive "
+            alt="banner"
+          />
+          <div>
+            <img
+              src={process.env.PUBLIC_URL + "/title.png"}
+              className="banner-title img-responsive "
+              alt="title"
+            />
+          </div>
+        </div>
+        <div className="title">
+          تسجيل الدخول <i className="fa-solid fa-right-to-bracket"></i>
+        </div>
+        <form className="login-form" onSubmit={handleSubmit}>
+          <label>
+            إسم المستخدم:
+            <input
+              type="text"
+              value={username}
+              onChange={(event) => setUserName(event.target.value)}
+              required
+            />
+          </label>
+          <label>
+            كلمة السر:
+            <input
+              type="password"
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+              required
+            />
+          </label>
+          <button type="submit">تسجيل الدخول</button>
+          {error && (
+            <div className="alert alert-primary pop" role="alert">
+              {error}
+            </div>
+          )}
+          <div>
+            لا تملك حسابًا وتريد بعمل حساب جديد؟{" "}
+            <Link to="/register">التسجيل</Link>
+          </div>
+        </form>
       </div>
-      <form className="login-form" onSubmit={handleSubmit}>
-        <label>
-          إسم المستخدم:
-          <input
-            type="text"
-            value={username}
-            onChange={(event) => setUserName(event.target.value)}
-            required
-          />
-        </label>
-        <label>
-          كلمة السر:
-          <input
-            type="password"
-            value={password}
-            onChange={(event) => setPassword(event.target.value)}
-            required
-          />
-        </label>
-        <button type="submit">تسجيل الدخول</button>
-        {error && <div className="alert alert-primary pop" role="alert">
-          {error}
-        </div>
-        }<div>
-          لا تملك حسابًا وتريد بعمل حساب جديد؟ <Link to="/register">التسجيل</Link>
-        </div>
-      </form>
     </>
-
   );
-
-
-
-
 }
 
 export default LoginForm;
--- src/Components/Registration/RegistrationForm.js
+++ src/Components/Registration/RegistrationForm.js
@@ -1,22 +1,21 @@
-import React, { useEffect, useState } from 'react';
-import "./RegistrationForm.css"
-import { Link, useNavigate } from 'react-router-dom';
-import { useCookies } from 'react-cookie';
+import React, { useEffect, useState } from "react";
+import "./RegistrationForm.css";
+import { Link, useNavigate } from "react-router-dom";
+import { useCookies } from "react-cookie";
 function RegistrationForm() {
-  const [username, setUserName] = useState('');
-  const [password, setPassword] = useState('');
-  const [sameUsername, setSameUserName] = useState(false)
+  const [username, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [sameUsername, setSameUserName] = useState(false);
   const navigate = useNavigate();
-  const [{ cookie }, setCookie] = useCookies(['user']);
+  const [ cookie , setCookie] = useCookies(["user"]);
   useEffect(() => {
-    if (cookie) {
-      // Redirect to login page if user data is not available
+    if (cookie.user) {
       navigate("/");
       return;
     }
-  }, [cookie, navigate])
+  }, [cookie, navigate]);
 
-  if (cookie) {
+  if (cookie.user) {
     return null;
   }
   const handleSubmit = async (event) => {
@@ -26,37 +25,45 @@ function RegistrationForm() {
       username: username,
       password: password,
     };
-    const response = await fetch(
-      `http://localhost:9000/register`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        credentials: 'include',
-        body: JSON.stringify({ data: formData }),
-      }
-    );
+    const response = await fetch(`http://localhost:9000/register`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify({ data: formData }),
+    });
     if (!response.ok) {
-      setSameUserName(true)
+      setSameUserName(true);
     } else {
-      setSameUserName(false)
+      setSameUserName(false);
       const userData = await response.json();
-      setCookie('user', JSON.stringify(userData))
+      setCookie("user", JSON.stringify(userData));
       navigate("/", {
         state: { user: userData },
-      })
+      });
     }
-
-
-
-  }
+  };
   return (
     <>
-      <div className='cover'>
-        <div className='title'>تسجيل حساب جديد</div>
-        <img src={process.env.PUBLIC_URL + '/logo.png'} className="img-responsive" alt="logo"/>
+      <div className="cover">
+        <img
+          src={process.env.PUBLIC_URL + "/banner.png"}
+          className="img-responsive "
+          alt="banner"
+        />
+        <div>
+          <img
+            src={process.env.PUBLIC_URL + "/title.png"}
+            className="banner-title img-responsive "
+            alt="title"
+          />
+        </div>
       </div>
+      <div className="title">
+        تسجيل حساب جديد <i className="fa-solid fa-address-card"></i>
+      </div>
+
       <div className="register-form">
         <form onSubmit={handleSubmit}>
           <label>
@@ -80,19 +87,17 @@ function RegistrationForm() {
             />
           </label>
           <button type="submit">سجل الآن!</button>
-          {sameUsername && <div className="alert alert-primary pop" role="alert">
-            هذا المستخدم موجود مسبقاً
-          </div>
-          }
+          {sameUsername && (
+            <div className="alert alert-primary pop" role="alert">
+              هذا المستخدم موجود مسبقاً
+            </div>
+          )}
           <div>
             هل تملك حسابًا ؟ <Link to="/login">تسجيل الدخول</Link>
           </div>
         </form>
       </div>
-
-
     </>
-
   );
 }
 
